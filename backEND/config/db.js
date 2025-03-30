aafcd02e2f1ff6610f590fd46db55641cfd4be73
mahmoud2408 <faresmahmoud806@gmail.com>
2025-03-30 10:21:57 +0100

    modification radhi

--- backEND/config/db.js
+++ backEND/config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: 3306,
+  port: 5001,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
