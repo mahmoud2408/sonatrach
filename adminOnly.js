6ede068c0cf72cdb22008e9da8b0933a036b0da4
mahmoud2408 <faresmahmoud806@gmail.com>
2025-04-04 00:13:31 +0100

    modif radhi

--- adminOnly.js
+++
@@ -1,9 +0,0 @@
-function adminOnly(req, res, next) {
-    if (req.session.userId && req.session.role === 'admin') {
-      return next();
-    }
-    return res.status(403).json({ error: "Accès refusé : administrateur uniquement." });
-  }
-  
-  module.exports = adminOnly;
-  
