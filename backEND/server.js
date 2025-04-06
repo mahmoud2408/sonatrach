d33fbab3c395ae8ad26f55158452700ba77d2a86
mahmoud2408 <faresmahmoud806@gmail.com>
2025-04-06 13:41:55 +0100

    radhi modif

--- backEND/server.js
+++ backEND/server.js
@@ -29,12 +29,14 @@ const adminActivityRoutes = require("./routes/adminActivity");
 const adminMemberRoutes = require("./routes/adminMember");
 const publicActivityRoutes = require("./routes/publicActivity");
 const users = require("./routes/users");
+const email = require("./routes/email");
 const memberSubscriptionRoutes = require("./routes/memberSubscription");
 const adminAdminRoutes = require("./routes/adminAdmin"); // Nouveau routeur pour créer admin
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminActivityRoutes);
 app.use("/api/admin", adminMemberRoutes);
+app.use("/api/email", email); // Monte le routeur "email"
 app.use("/api/activities", publicActivityRoutes);
 app.use("/api/members", memberSubscriptionRoutes);
 app.use("/api/users", users); // Monte le routeur "users"
